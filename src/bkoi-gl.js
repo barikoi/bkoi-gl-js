7e06c8d7153555390c2e50a61bd72ba43ae89448
scarecrow-11 <hafizurmilon.11@gmail.com>
2021-04-19 16:42:43 +0600

    Added Switchable Map Styles & GL Draw.

--- src/bkoi-gl.js
+++ src/bkoi-gl.js
@@ -1,11 +1,17 @@
-import { Map, NavigationControl, GeolocateControl, Popup, Marker } from 'maplibre-gl'
+import { Map, NavigationControl, GeolocateControl, ScaleControl, Popup, Marker } from 'maplibre-gl'
+
+// Default Style Types
+const defaultStyleTypes = {
+  light: 'https://map.barikoi.com/styles/osm-liberty/style.json',
+  dark: 'https://map.barikoi.com/styles/barikoi-dark/style.json'
+}
 
 // Extend Map
 class BkoiGlMap extends Map {
   constructor(mapOptions) {
     super({
       ...mapOptions,
-      style: 'https://map.barikoi.com/styles/osm-liberty/style.json'
+      style: defaultStyleTypes[ mapOptions.styleType ] ?? defaultStyleTypes.light
     })
   }
 }
@@ -15,6 +21,7 @@ export {
   BkoiGlMap as Map,
   NavigationControl,
   GeolocateControl,
+  ScaleControl,
   Popup,
   Marker
 }
