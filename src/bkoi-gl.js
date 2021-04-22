c911d5fda6cb3b0e4777c20e09c8aa24624ada03
scarecrow-11 <hafizurmilon.11@gmail.com>
2021-04-22 16:57:48 +0600

    Fixed switchable style types.

--- src/bkoi-gl.js
+++ src/bkoi-gl.js
@@ -1,4 +1,4 @@
-import { Map, NavigationControl, GeolocateControl, ScaleControl, Popup, Marker } from 'maplibre-gl'
+import { Map, NavigationControl, GeolocateControl, ScaleControl, Popup, Marker, FullscreenControl } from 'maplibre-gl'
 
 // Default Style Types
 const defaultStyleTypes = {
@@ -9,10 +9,28 @@ const defaultStyleTypes = {
 // Extend Map
 class BkoiGlMap extends Map {
   constructor(mapOptions) {
+    if(!mapOptions.styleType || !defaultStyleTypes[ mapOptions.styleType ]) {
+      console.warn("Invalid Style Type. Please choose from [ 'light', 'dark' ]. Setting 'light' as fallback.")
+    }
+
     super({
       ...mapOptions,
-      style: defaultStyleTypes[ mapOptions.styleType ] ?? defaultStyleTypes.light
+      style: mapOptions.style ?
+        mapOptions.style :
+        (mapOptions.styleType && defaultStyleTypes[ mapOptions.styleType ]) ?
+        defaultStyleTypes[ mapOptions.styleType ] :
+        defaultStyleTypes.light
     })
+  }
+
+  // Set Map Style based on styleType
+  setStyleType(styleType) {
+    if(styleType && defaultStyleTypes[ styleType ]) {
+      this.setStyle(defaultStyleTypes[ styleType ])
+
+    } else {
+      throw new Error("Invalid Style Type. Please choose from [ 'light', 'dark' ].")
+    }
   }
 }
 
@@ -23,5 +41,6 @@ export {
   GeolocateControl,
   ScaleControl,
   Popup,
-  Marker
+  Marker,
+  FullscreenControl
 }
