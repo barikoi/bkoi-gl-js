c0fccd061207761ab981414ca653d804aef94d19
scarecrow-11 <hafizurmilon.11@gmail.com>
2022-03-16 15:30:24 +0600

    Updated NPM build.

--- rollup.config.js
+++ rollup.config.js
@@ -12,7 +12,7 @@ export default [
     {
         input: 'src/index.js',
         output: {
-            file: 'dist/bkoi-gl.js',
+            file: 'dist/iife/bkoi-gl.js',
             format: 'iife',
             name: 'bkoigl',
             globals: {
@@ -32,7 +32,41 @@ export default [
             terser(),
             copy({
                 targets: [
-                    { src: 'src/index.css', dest: 'dist', rename: 'bkoi-gl.css' }
+                    { src: 'src/index.css', dest: 'dist/iife', rename: 'bkoi-gl.css' }
+                ]
+            })
+        ]
+    },
+    {
+        input: 'src/index.js',
+        external: [ /@babel\/runtime/, 'maplibre-gl' ],
+        output: [
+            {
+                file: 'dist/cjs/bkoi-gl.js',
+                format: 'cjs',
+                exports: 'auto'
+            },
+            {
+                file: 'dist/esm/bkoi-gl.js',
+                format: 'es',
+                exports: 'auto'
+            }
+        ],
+        plugins: [
+            clear({ targets: [ 'dist/cjs', 'dist/esm', 'dist/style' ] }),
+            nodeResolve(),
+            commonjs(),
+            image(),
+            eslint(),
+            babel({
+                exclude: 'node_modules/**',
+                babelHelpers: 'runtime',
+                plugins: [ '@babel/plugin-transform-runtime' ]
+            }),
+            terser(),
+            copy({
+                targets: [
+                    { src: 'src/index.css', dest: 'dist/style', rename: 'bkoi-gl.css' }
                 ]
             })
         ]
